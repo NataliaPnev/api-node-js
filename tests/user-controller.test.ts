8f81fbbadd687eaad514b1bc642960900709afd0
User <nataliapnevskaya@gmail.com>
2025-07-09 14:24:53 +0300

    Revert "Added http-status-codes in devDependencies"
    This reverts commit cc6af1ac9b2d403fe8038900995b46a46b7d9cf6.

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -4,6 +4,14 @@ import {StatusCodes} from "http-status-codes";
 let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API', () => {
+
+    test('all users: should return empty array when no users', async ({ request }) => {
+        const response = await request.get(`${baseURL}`);
+        expect(response.status()).toBe(StatusCodes.OK);
+        const responseBody = await response.text()
+        expect(responseBody).toBe('[]');
+    });
+
     test('find user: should return a user by ID', async ({ request }) => {
         const responseCreate = await request.post(`${baseURL}`);
         const responseBody = await responseCreate.json()
